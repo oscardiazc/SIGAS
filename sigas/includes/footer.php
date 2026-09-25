52bd848f2fc6160d9c15036cfc020ec7a057d6d7
oscardiazc <oscardiaz5516@gmail.com>
2026-09-25 10:10:10 -0300

    Delete sigas directory

--- sigas/includes/footer.php
+++
@@ -1,8 +0,0 @@
-<?php
-// PIE DE PAGINA COMUN DEL SISTEMA
-?>
-    <footer class="footer-principal">
-        <p>SIGAS - Sistema Gestión Solicitudes de Atencion de Salud &copy; <?= date('Y') ?></p>
-    </footer>
-</body>
-</html>
